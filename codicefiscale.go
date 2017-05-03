7c1986feb97798028e6ad02489d83e90da8063d7
Marco su Acerrimo3 <mbanfi@gmail.com>
2017-05-03 13:17:34 +0200

    modified:   codicefiscale.go modificati commenti modified:   codicefiscale_test.go incluso "" come valido.

--- codicefiscale.go
+++ codicefiscale.go
@@ -21,12 +21,16 @@ func (cf *CFError) Error() string {
 	return cf.msg
 }
 
-//CodiceFiscale  controlla il codice fiscale, restituisce doppio valore, true/false e messaggio ove opportuno
-//si aspetta in ingresso il codice fiscale MAIUSCOLO
+//CodiceFiscale  controlla il codice fiscale, restituisce doppio valore, true/false e messaggio, ove opportuno
+//si aspetta in ingresso il codice fiscale, non importa il case (viene convertito in maiuscolo)
+//se cfin è vuota, viene considerata valida, per questo caso, il controllo dovrebbe essere altrove
 func CodiceFiscale(cfin string) (bool, *CFError) {
+	if len(cfin) == 0 {
+		return true,nil
+	}
 	if len(cfin) != 16 {
 		er := new(CFError)
-		er.msg = "Lunghezza Errata"
+		er.msg = "Wrong Length"
 		return false, er
 	}
 	//decodifica carattere di controllo cf
@@ -36,19 +40,19 @@ func CodiceFiscale(cfin string) (bool, *CFError) {
 		"I": 19, "J": 21, "K": 2, "L": 4, "M": 18, "N": 20, "O": 11, "P": 3, "Q": 6, "R": 8,
 		"S": 12, "T": 14, "U": 16, "V": 10, "W": 22, "X": 25, "Y": 24, "Z": 23,
 	}
-	//ordinamento - più semplice di int(rune) - int('A') oppure int('0')
+	//map per simulare "ord" di altri linguaggi - più semplice di int(rune) - int('A') oppure int('0')
 	ordv := map[string]int{
 		"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
-		"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7,
-		"I": 8, "J": 9, "K": 10, "L": 11, "M": 12, "N": 13, "O": 14, "P": 15, "Q": 16, "R": 17,
-		"S": 18, "T": 19, "U": 20, "V": 21, "W": 22, "X": 23, "Y": 24, "Z": 25,
+		"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7, "I": 8, "J": 9,
+		"K": 10, "L": 11, "M": 12, "N": 13, "O": 14, "P": 15, "Q": 16, "R": 17, "S": 18,
+		"T": 19, "U": 20, "V": 21, "W": 22, "X": 23, "Y": 24, "Z": 25,
 	}
 	cfin = strings.ToUpper(cfin)
 	//verifica per simboli inattesi
 	for _, v := range cfin {
 		if _, ok := ordv[string(v)]; !ok {
 			er := new(CFError)
-			er.msg = "Simboli non validi"
+			er.msg = "Invalid chars"
 			return false, er
 		}
 	}
@@ -59,7 +63,7 @@ func CodiceFiscale(cfin string) (bool, *CFError) {
 	}
 	if s%26 != ordv[string(cfin[15])] {
 		er := new(CFError)
-		er.msg = "Codice Di Controllo"
+		er.msg = "Wrong Check Code"
 		return false, er
 	}
 	return true, nil
--- codicefiscale_test.go
+++ codicefiscale_test.go
@@ -9,14 +9,14 @@ import (
 
 func TestCodiceFiscale(t *testing.T) {
 	//valido
-	testOK := []string{"ABCDEF12B23P432P", "MROrSs00a00A000U"}
+	testOK := []string{"ABCDEF12B23P432P", "MROrSs00a00A000U",""}
 	testKO := []string{"ABCDEF12B23P432X", "MRORSS00A00A000V", "MROrSs00a00-A00U", "MRORSS00A.+A000V", "MROrSs00a00A000"}
 	for _, v := range testOK {
 		s, err := CodiceFiscale(v)
 		if !s {
-			t.Fatal("Error! Codice Fiscale", v, " should be valid")
+			t.Fatal("Ko: Error! Codice Fiscale", v, " should be valid", err)
 		}
-		fmt.Println("ok:", v, "M:", err)
+		fmt.Printf("Ok (valid) \"%s\"\n", v)
 	}
 	//codici non validi
 	for _, v := range testKO {
@@ -25,6 +25,6 @@ func TestCodiceFiscale(t *testing.T) {
 		if err == nil {
 			t.Fatal("Error! Codice Fiscale", v, " should be invalid")
 		}
-		fmt.Println("ok:", v, "M:", err)
+		fmt.Printf("Ok (invalid) \"%s\",%s\n", v,err)
 	}
 }
