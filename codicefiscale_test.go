703c3e97aa525baac848f2f441a01c54d4421983
Marco su Acerrimo3 <mbanfi@gmail.com>
2017-05-06 10:04:55 +0200

    modified:   codicefiscale_test.go uniformato all'italiano i commenti ed i messaggi di test

--- codicefiscale_test.go
+++ codicefiscale_test.go
@@ -8,24 +8,24 @@ import (
 )
 
 func TestCodiceFiscale(t *testing.T) {
-	//codici validi
+	//codici validi (formalmente - ogni somiglianza con eventuali codici reali è casuale)
 	testOK := []string{"ABCDEF12B23P432P", "MROrSs00a00A000U", ""}
 	//codici non validi
 	testKO := []string{"ABCDEF12B23P432X", "MRORSS00A00A000V", "MROrSs00a00-A00U", "MRORSS00A.+A000V", "MROrSs00a00A000"}
 	for _, v := range testOK {
 		s, err := CodiceFiscale(v)
 		if !s {
-			t.Fatal("Ko: Error! Codice Fiscale", v, " should be valid", err)
+			t.Fatal("Ko. Errore, Il Codice Fiscale", v, " dovrebbe essere valido", err)
 		}
-		fmt.Printf("Ok (valid) \"%s\"\n", v)
+		fmt.Printf("Ok (valido) \"%s\"\n", v)
 	}
 	//codici non validi
 	for _, v := range testKO {
 		_, err := CodiceFiscale(v)
 
 		if err == nil {
-			t.Fatal("Error! Codice Fiscale", v, " should be invalid")
+			t.Fatal("Ko. Errore, Il Codice Fiscale", v, " NON dovrebbe essere valido")
 		}
-		fmt.Printf("Ok (invalid) \"%s\",%s\n", v, err)
+		fmt.Printf("Ok (non valido) \"%s\",%s\n", v, err)
 	}
 }
